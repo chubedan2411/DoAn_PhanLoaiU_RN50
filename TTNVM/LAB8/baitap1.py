2ed238353d5f12fd56b91f0963cf38c612927b05
Trần Ken <126503089+chubedan2411@users.noreply.github.com>
2025-07-04 00:52:52 +0700

    Delete TTNVM directory

--- TTNVM/LAB8/baitap1.py
+++
@@ -1,6 +0,0 @@
-def reverse_string():
-    user_input = input("Nhập một chuỗi: ")
-    reversed_string = user_input[::-1]
-    print(f"Chuỗi đảo ngược là: {reversed_string}")
-if __name__ == "__main__":
-    reverse_string()
